0354fee2b52be928c8e1bec553d65296972ac668
glbter <nicknema13@gmail.com>
2021-04-14 14:18:39 +0300

    update: moved test function to test_data.py

--- lab5/test_data.py
+++ lab5/test_data.py
@@ -1,3 +1,7 @@
+import numpy as np
+from mutlistep_methods import AdamsMethods
+
+
 def dy_f(x, y, z):
     return z
 
@@ -16,3 +20,17 @@ def test_dy_f(x, y, z):
 
 def test_dz_g(x, y, z):
     return (2*x*z) / (x**2 + 1)
+
+
+def test():
+    test_dy_f = lambda x, y, z: z
+    test_dz_g = lambda x, y, z: (2 * x * y) / (x ** 2 + 1)
+    func = np.asarray([1, 1.607999216, 2.263994646, 3.015985963, 3.911973624, 5]).flatten()
+    res = AdamsMethods.adams_method(test_dy_f, test_dz_g, 0.2, 0, 1, 3, 1)
+    print(res)
+    print(np.fabs(np.subtract(res[:, 1:2].flatten(), func)))
+    print()
+
+    res = AdamsMethods.adams_boshfort_method(test_dy_f, test_dz_g, 0.2, 0, 1, 3, 1)
+    print(res)
+    print(np.fabs(np.subtract(res[:, 1:2].flatten(), func)))
--- lab5/utils.py
+++ lab5/utils.py
@@ -1,6 +1,4 @@
-import numpy as np
 from math import floor, fabs, ceil, log10
-from mutlistep_methods import AdamsMethods
 
 
 def drange(start, stop, step):
@@ -11,17 +9,3 @@ def drange(start, stop, step):
         yield r
         r += step
         r = ceil(r * 10 ** n) / 10 ** n
-
-
-def test():
-    test_dy_f = lambda x, y, z: z
-    test_dz_g = lambda x, y, z: (2 * x * y) / (x ** 2 + 1)
-    func = np.asarray([1, 1.607999216, 2.263994646, 3.015985963, 3.911973624, 5]).flatten()
-    res = AdamsMethods.adams_method(test_dy_f, test_dz_g, 0.2, 0, 1, 3, 1)
-    print(res)
-    print(np.fabs(np.subtract(res[:, 1:2].flatten(), func)))
-    print()
-
-    res = AdamsMethods.adams_boshfort_method(test_dy_f, test_dz_g, 0.2, 0, 1, 3, 1)
-    print(res)
-    print(np.fabs(np.subtract(res[:, 1:2].flatten(), func)))
